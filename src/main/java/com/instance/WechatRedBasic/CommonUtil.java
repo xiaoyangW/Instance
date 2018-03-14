88575e057aedb3ea4353ea0e949c27963c674370
xiaoyang <1483863489@qq.com>
2018-03-14 13:38:34 +0800

    update

--- src/main/java/com/instance/WechatRedBasic/CommonUtil.java
+++ src/main/java/com/instance/WechatRedBasic/CommonUtil.java
@@ -1,23 +1,10 @@
 package com.instance.WechatRedBasic;
 
-import com.alibaba.fastjson.JSONException;
-import com.alibaba.fastjson.JSONObject;
-import com.instance.util.StringUtil;
-import com.sof.brokerwechat.model.Token;
-import org.apache.log4j.Logger;
-import org.jdom.Document;
-import org.jdom.Element;
-import org.jdom.JDOMException;
-import org.jdom.input.SAXBuilder;
 
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSocketFactory;
-import javax.net.ssl.TrustManager;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.input.SAXBuilder;
 import java.io.*;
-import java.net.ConnectException;
-import java.net.URL;
-import java.sql.Timestamp;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -31,159 +18,27 @@ import java.util.Map;
  */
 public class CommonUtil {
 
-    /**
-     * 发送https请求
-     *
-     * @param requestUrl    请求地址
-     * @param requestMethod 请求方式（GET、POST）
-     * @param outputStr     提交的数据
-     * @return JSONObject(通过JSONObject.get(key)的方式获取json对象的属性值)
-     */
-    public static String httpsRequest(String requestUrl, String requestMethod, String outputStr) {
-        JSONObject jsonObject = null;
-        String ReturnsData = "";
-        try {
-            // 创建SSLContext对象，并使用我们指定的信任管理器初始化
-            TrustManager[] tm = {new MyX509TrustManager()};
-            SSLContext sslContext = SSLContext.getInstance("SSL", "SunJSSE");
-            sslContext.init(null, tm, new java.security.SecureRandom());
-            // 从上述SSLContext对象中得到SSLSocketFactory对象
-            SSLSocketFactory ssf = sslContext.getSocketFactory();
-            URL url = new URL(requestUrl);
-            HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
-            conn.setSSLSocketFactory(ssf);
-            conn.setDoOutput(true);
-            conn.setDoInput(true);
-            conn.setUseCaches(false);
-            // 设置请求方式（GET/POST）
-            conn.setRequestMethod(requestMethod);
-            // 当outputStr不为null时向输出流写数据
-            if (null != outputStr) {
-                OutputStream outputStream = conn.getOutputStream();
-                // 注意编码格式
-                outputStream.write(outputStr.getBytes("UTF-8"));
-                outputStream.close();
-            }
-            // 从输入流读取返回内容
-            InputStream inputStream = conn.getInputStream();
-            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "utf-8");
-            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-            String str = null;
-            StringBuffer buffer = new StringBuffer();
-            while ((str = bufferedReader.readLine()) != null) {
-                buffer.append(str);
-            }
-            // 释放资源
-            bufferedReader.close();
-            inputStreamReader.close();
-            inputStream.close();
-            inputStream = null;
-            conn.disconnect();
-            jsonObject = JSONObject.parseObject(buffer.toString());
-            ReturnsData = buffer.toString();
-        } catch (ConnectException ce) {
-            log.error("连接超时：{}", ce);
-        } catch (Exception e) {
-            log.error("https请求异常：{}", e);
-        }
-        return ReturnsData;
-    }
-
-    /**
-     * 获取接口访问凭证
-     *
-     * @param appid     凭证
-     * @param appsecret 密钥
-     * @return
-     */
-    public static Token getToken(String appid, String appsecret) {
-        Token token = null;
-        String requestUrl = StringUtil.TOKEN_URL.replace("APPID", appid).replace("APPSECRET", appsecret);
-        // 发起GET请求获取凭证
-        JSONObject jsonObject = JSONObject.parseObject(httpsRequest(requestUrl, "GET", null));
-
-        if (null != jsonObject) {
-            try {
-                token = new Token();
-                token.setAccess_token(jsonObject.getString("access_token"));
-                token.setExpires_in(jsonObject.getInteger("expires_in"));
-                token.setAppid(StringUtil.APPID);
-                token.setErrcode(jsonObject.getString("errcode"));
-                token.setErrcode(jsonObject.getString("errmsg"));
-                token.setCreate_time(new Timestamp(System.currentTimeMillis()));
-            } catch (JSONException e) {
-                token = null;
-                // 获取token失败
-            }
-        }
-        return token;
-    }
-
-    /**
-     * 获取jsapi ticket
-     *
-     * @param access_token
-     * @return
-     */
-    public static Token getJsapi_ticket(String access_token) {
-        Token token = null;
-        String requestUrl = StringUtil.JSAPI_TICKET.replace("ACCESS_TOKEN", access_token);
-        // 发起GET请求获取凭证
-        JSONObject jsonObject = JSONObject.parseObject(httpsRequest(requestUrl, "GET", null));
-
-        if (null != jsonObject) {
-            try {
-                token = new Token();
-                token.setAccess_token(jsonObject.getString("ticket"));
-                token.setExpires_in(jsonObject.getInteger("expires_in"));
-                token.setAppid(StringUtil.APPID);
-                token.setErrcode(jsonObject.getString("errcode"));
-                token.setErrcode(jsonObject.getString("errmsg"));
-                token.setCreate_time(new Timestamp(System.currentTimeMillis()));
-            } catch (JSONException e) {
-                token = null;
-            }
-        }
-        return token;
-    }
-
-    /**
-     * URL编码（utf-8）
-     *
-     * @param source
-     * @return
-     */
-    public static String urlEncodeUTF8(String source) {
-        String result = source;
-        try {
-            result = java.net.URLEncoder.encode(source, "utf-8");
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-        return result;
-    }
 
     /**
      * 解析xml,返回第一级元素键值对。如果第一级元素有子节点，则此节点的值是子节点的xml数据。
      *
-     * @param strxml
+     * @param strxml xml string
      */
-    public static Map doXMLParse(String strxml) throws Exception {
+    public static Map<String, String> doXMLParse(String strxml) throws Exception {
         if (null == strxml || "".equals(strxml)) {
             return null;
         }
-        Map m = new HashMap();
-        InputStream in = String2Inputstream(strxml);
+        Map<String, String> m = new HashMap<String, String>();
+        InputStream in = new ByteArrayInputStream(strxml.getBytes());
         SAXBuilder builder = new SAXBuilder();
         Document doc = builder.build(in);
         Element root = doc.getRootElement();
-        List list = root.getChildren();
-        Iterator it = list.iterator();
-        while (it.hasNext()) {
-            Element e = (Element) it.next();
+        List<Element> list = root.getChildren();
+        for (Object aList : list) {
+            Element e = (Element) aList;
             String k = e.getName();
             String v = "";
-            List children = e.getChildren();
+            List<Element> children = e.getChildren();
             if (children.isEmpty()) {
                 v = e.getTextNormalize();
             } else {
@@ -193,7 +48,6 @@ public class CommonUtil {
         }
         // 关闭流
         in.close();
-
         return m;
     }
 
@@ -203,28 +57,22 @@ public class CommonUtil {
      * @param children
      * @return String
      */
-    public static String getChildrenText(List children) {
-        StringBuffer sb = new StringBuffer();
+    public static String getChildrenText(List<Element> children) {
+        StringBuilder sb = new StringBuilder();
         if (!children.isEmpty()) {
-            Iterator it = children.iterator();
-            while (it.hasNext()) {
-                Element e = (Element) it.next();
+            for (Element e : children) {
                 String name = e.getName();
                 String value = e.getTextNormalize();
-                List list = e.getChildren();
-                sb.append("<" + name + ">");
+                List<Element> list = e.getChildren();
+                sb.append("<").append(name).append(">");
                 if (!list.isEmpty()) {
                     sb.append(getChildrenText(list));
                 }
                 sb.append(value);
-                sb.append("</" + name + ">");
+                sb.append("</").append(name).append(">");
             }
         }
         return sb.toString();
-    }
-
-    public static InputStream String2Inputstream(String str) {
-        return new ByteArrayInputStream(str.getBytes());
     }
 
 }
